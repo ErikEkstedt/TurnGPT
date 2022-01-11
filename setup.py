3083a745803e109d08ead15be8638da663b276ff
Erik <eeckee@gmail.com>
2022-01-11 12:52:04 +0100

    clean

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="turngpt",
-    version="0.0.0",
+    version="0.1.0",
     description="turngpt",
     author="erikekst",
     author_email="erikekst@kth.se",
-    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="",
-    packages=find_packages(),
+    url="https://github.com/ErikEkstedt/TurnGPT",
+    packages=["turngpt"],
 )
