64f7d8c3b00e2a8332bca9267e6252462cbd0407
Erik <eeckee@gmail.com>
2022-10-02 18:26:10 +0200

    BREAKING -> Update datasets_turntaking -> branch main_new_text (for now)

--- turngpt/train.py
+++ turngpt/train.py
@@ -6,7 +6,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 import pytorch_lightning as pl
 
-from datasets_turntaking import ConversationalDM
+from datasets_turntaking import DialogTextDM
 from turngpt.model import TurnGPT, TurnGPTWandbCallbacks
 
 
@@ -50,7 +50,7 @@ def default_logger_callbacks(name, args, callbacks):
 def train():
     parser = ArgumentParser()
     parser = TurnGPT.add_model_specific_args(parser)
-    parser = ConversationalDM.add_data_specific_args(parser)
+    parser = DialogTextDM.add_data_specific_args(parser)
     parser = pl.Trainer.add_argparse_args(parser)
     parser.add_argument("--seed", type=int, default=1)
     parser.add_argument("--name_info", type=str, default="")
@@ -82,7 +82,7 @@ def train():
     model.print_parameters()
 
     # DataModule
-    dm = ConversationalDM(
+    dm = DialogTextDM(
         datasets=args.datasets,
         tokenizer=model.tokenizer,
         batch_size=args.batch_size,
